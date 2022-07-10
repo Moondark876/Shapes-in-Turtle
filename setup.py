49eeb48fe59088a6c39fadd16a155737bb12ebec
Moondark876 <tjonsmith64@gmail.com>
2022-07-10 02:06:38 -0500

    Updated setup.py to function

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='Shapes',
-      version='1.0',
-      description='Library for Shapes in Python using turtle',
-      author='Moondark876',
-      url='https://github.com/Moondark876/Shapes-in-Turtle',
-      packages=['turtle'],
-     )
+
+def readme_text():
+    with open('README.md') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name='Shapes',
+    packages=['shapes'],
+    version='1.0.0',
+    description='Library for Shapes in Python using turtle',
+    long_description=readme_text(),
+    author='Moondark876',
+    url='https://github.com/Moondark876/Shapes-in-Turtle',
+    install_requires=['turtle'],
+)
